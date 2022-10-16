cc4925f64061c4d9792c05f80e004a390119015b
jakovid <jacobk.dowdle@gmail.com>
2022-10-16 14:34:58 +0800

    began to create project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
+import { render } from "@testing-library/react";
+import React, { Component, component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    
+    this.state = {
+      general: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        phone: '',
+      },
+      education: {
+        school: '',
+        major: '',
+        graduation: '',
+      },
+      experience: {
+
+      }
+
+    }
+  }
+
+  render() {
+
+    return(
+      <div>Hello</div>
+    )
+  }
 }
 
 export default App;
